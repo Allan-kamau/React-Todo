9fbb7a8fc2442ad602357fce231b6ea2a9ec89b6
kamau41 <41kamau41@gmail.com>
2020-08-03 20:56:11 +0300

    added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import _ from "lodash";
+import axios from "axios";
 
 function App() {
   const [todos, setTodos] = useState([]);
@@ -25,7 +26,7 @@ function App() {
     );
   }
   function removeTodo(id) {
-    setTodos(todos.filter((todo) => todo.id !== id));
+    // setTodos(todos.filter((todo) => todo.id !== id));
   }
   return (
     <div className="App">
@@ -44,11 +45,12 @@ function App() {
               <div className={"column"}>
                 <h3>{data.title}</h3>
                 <Droppable droppableId={key}>
-                  {(provided, snapshot) => {
+                  {(provided) => {
                     return (
                       <div
                         ref={provided.innerRef}
                         {...provided.dropplableProps}
+                        className={"droppable-col"}
                       ></div>
                     );
                   }}
@@ -63,3 +65,71 @@ function App() {
 }
 
 export default App;
+
+// import React, { useState } from "react";
+// // import logo from "./logo.svg";
+// import "./App.css";
+// import TodoForm from "./components/TodoForm";
+// import TodoList from "./components/TodoList";
+// import { DragDropContext, Droppable } from "react-beautiful-dnd";
+// import _, { initial } from "lodash";
+
+// function App() {
+//   const [todos, setTodos] = useState({});
+
+//   function addTodo(todo) {
+//     setTodos([todo, ...todos]);
+//   }
+//   function toggleComplete(id) {
+//     setTodos(
+//       todos.map((todo) => {
+//         if (todo.div === id) {
+//           return {
+//             ...todo,
+//             completed: !todo.completed,
+//           };
+//         }
+//         return todo;
+//       })
+//     );
+//   }
+//   function removeTodo(id) {
+//     setTodos(todos.filter((todo) => todo.id !== id));
+//   }
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>ToDo lIsT aPp</p>
+
+//         <TodoForm addTodo={addTodo} />
+//         <TodoList
+//           todos={todos}
+//           toggleComplete={toggleComplete}
+//           removeTodo={removeTodo}
+//         />
+//         <DragDropContext onDragEnd={(e) => console.log(e)}>
+//           {_.map(todos, (data, key) => {
+//             return (
+//               <div className={"column"}>
+//                 <h3>{data.title}</h3>
+//                 <Droppable droppableId={key}>
+//                   {(provided) => {
+//                     return (
+//                       <div
+//                         ref={provided.innerRef}
+//                         {...provided.dropplableProps}
+//                         className={"droppable-col"}
+//                       ></div>
+//                     );
+//                   }}
+//                 </Droppable>
+//               </div>
+//             );
+//           })}
+//         </DragDropContext>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
